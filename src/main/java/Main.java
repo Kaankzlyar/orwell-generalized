e70abb0e3668ffc88da510a37ce7f1fd8881b86b
João Lourenço <93186722+Tonevanda@users.noreply.github.com>
2026-02-19 17:24:49 +0000

    start get request to AR

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Main {
-    public static void main(String[] args) {
-       System.out.print("Hello and welcome!");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        var client = HttpClient.newHttpClient();
+
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create("https://app.parlamento.pt/webutils/docs/doc.xml?path=N3jRPJFnnPw07UGV%2f4k0mwzgbG2Jl3wjBTiC8TM1giRsecyPDp4yf3k8FPfa4jKxK8cK%2bHg9qZBa2M5gomJKy%2fQtnk5tb5Z5r8BVFPJMhxvRcyvUOUPpY%2bt87ZuUHz9BvAd789KDSyluIGUKEnTFo9Dpn1Ykljq4eeqISeeUTJT5D1DUC%2f%2biDqKA553hfRTTsEe86A1ObPNZtwCqvv05dxSywmi%2ftOnvfCrhUXy1NfIbEKmFTBEf0i6Ucx0Up0bYMVphP5VomK8C%2bxF7onHEFOpMyRuY9v81e%2bKpmj%2fFjokJsrEnU%2baxPEeUTE9%2bmeOp2g6zC2kPSEZAn7%2fJWt4sioTjDjQ20aK58Q4QBUcFIkw%3d&fich=InformacaoBaseXV.xml&Inline=true"))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
     }
 }
