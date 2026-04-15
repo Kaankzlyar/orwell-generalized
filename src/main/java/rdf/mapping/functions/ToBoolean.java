0ef85df6e786c6bfbeee84c6e7b887b0f16f6c00
João Lourenço <93186722+Tonevanda@users.noreply.github.com>
2026-04-16 00:07:11 +0100

    mapping: bio:title

--- src/main/java/rdf/mapping/functions/ToBoolean.java
+++ src/main/java/rdf/mapping/functions/ToBoolean.java
@@ -4,14 +4,11 @@ public class ToBoolean {
 
     public static Boolean toBoolean(String stringValue) {
         
-        System.out.println("Received input: '" + stringValue + "'"); // Debugging output
         if (stringValue == null) {
             return null;
         }
 
         String normalized = stringValue.trim().toLowerCase();
-
-        System.out.println("Normalized input: '" + normalized + "'"); // Debugging output
 
         // Add more cases if needed
         switch (normalized) {
