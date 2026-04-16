3a95c075a1576f456a04dcef62b707d490b15a41
João Lourenço <93186722+Tonevanda@users.noreply.github.com>
2026-04-16 17:55:05 +0100

    mapping: use parenttriplesmap

--- src/main/java/rdf/mapping/functions/ToHabilitationLevel.java
+++ src/main/java/rdf/mapping/functions/ToHabilitationLevel.java
@@ -4,15 +4,17 @@ import java.util.Map;
 
 public class ToHabilitationLevel {
 
+    private static final String BIO_NS = "http://purl.org/polis/ar/biographic#";
+
     private static final Map<String, String> habilitationLevelMap = Map.of(
-        "9.0", "PrimarySchool",
-        "10.0", "MiddleSchool",
-        "11.0", "EarlyHighSchool",
-        "12.0", "HighSchool",
-        "13.0", "HigherEducation",
-        "14.0", "Bachelor",
-        "15.0", "Master",
-        "16.0", "Postgrad"  
+        "9.0", BIO_NS + "PrimarySchool",
+        "10.0", BIO_NS + "MiddleSchool",
+        "11.0", BIO_NS + "EarlyHighSchool",
+        "12.0", BIO_NS + "HighSchool",
+        "13.0", BIO_NS + "HigherEducation",
+        "14.0", BIO_NS + "Bachelor",
+        "15.0", BIO_NS + "Master",
+        "16.0", BIO_NS + "Postgrad"  
     );
 
     public static String toHabilitationLevel(String inputHabilitationLevel){
