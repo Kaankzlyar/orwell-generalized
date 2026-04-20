f69282d84b6f1e7171fee72ce55108cbb79c016c
João Lourenço <jmguellourenco@gmail.com>
2026-04-20 21:21:44 +0100

    mapping: ypa with sha1 hash and array join

--- src/main/java/rdf/mapping/functions/ToEnglish.java
+++ src/main/java/rdf/mapping/functions/ToEnglish.java
@@ -7,6 +7,7 @@ import java.util.Locale;
 public class ToEnglish {
 
     private static final String POLIS_CORE_NS = "http://purl.org/polis/ar/core#";
+    private static final String POLIS_MPACT_NS = "http://purl.org/polis/ar/mp-activity#";
     private static final HashMap<String, String> situationMap = new HashMap<>();
     private static final HashMap<String, String> dutyMap = new HashMap<>();
     private static final HashMap<String, String> schoolTypeMap = new HashMap<>();
@@ -18,32 +19,33 @@ public class ToEnglish {
     }
 
     private static void fillSituationMap() {
-        situationMap.put("desistencia", "Withdrawal");
-        situationMap.put("efetivo", "Incumbent");
-        situationMap.put("efetivo definitivo", "PermanentIncumbent");
-        situationMap.put("efetivo temporario", "TemporaryIncumbent");
-        situationMap.put("falecido/a", "Deceased");
-        situationMap.put("impedido", "Disqualified");
-        situationMap.put("perda de mandato", "LossOfMandate");
-        situationMap.put("renunciou", "Resigned");
-        situationMap.put("suplente", "Alternate");
-        situationMap.put("suspenso", "Suspended");
-        situationMap.put("suspenso(eleito)", "Suspended");
-        situationMap.put("suspenso(efet def)", "Suspended");
-        situationMap.put("suspenso(nao eleito)", "Suspended");
+        situationMap.put("desistencia", POLIS_CORE_NS + "Withdrawal");
+        situationMap.put("efetivo", POLIS_CORE_NS + "Incumbent");
+        situationMap.put("efetivo definitivo", POLIS_CORE_NS + "PermanentIncumbent");
+        situationMap.put("efetivo temporario", POLIS_CORE_NS + "TemporaryIncumbent");
+        situationMap.put("falecido/a", POLIS_CORE_NS + "Deceased");
+        situationMap.put("impedido", POLIS_CORE_NS + "Disqualified");
+        situationMap.put("perda de mandato", POLIS_CORE_NS + "LossOfMandate");
+        situationMap.put("renunciou", POLIS_CORE_NS + "Resigned");
+        situationMap.put("suplente", POLIS_CORE_NS + "Alternate");
+        situationMap.put("suspenso", POLIS_CORE_NS + "Suspended");
+        situationMap.put("suspenso(eleito)", POLIS_CORE_NS + "Suspended");
+        situationMap.put("suspenso(efet def)", POLIS_CORE_NS + "Suspended");
+        situationMap.put("suspenso(nao eleito)", POLIS_CORE_NS + "Suspended");
     }
 
     private static void fillDutyMap() {
-        dutyMap.put("presidente", "PAR");
-        dutyMap.put("vice-presidente", "VicePAR");
-        dutyMap.put("secretario", "Secretary");
-        dutyMap.put("vice-secretario", "ViceSecretary");
+        dutyMap.put("presidente", POLIS_CORE_NS + "PAR");
+        dutyMap.put("vice-presidente", POLIS_CORE_NS + "VicePAR");
+        dutyMap.put("secretario", POLIS_CORE_NS + "Secretary");
+        dutyMap.put("vice-secretario", POLIS_CORE_NS + "ViceSecretary");
     }
 
     private static void fillSchoolTypeMap() {
-        schoolTypeMap.put("basico", "Basic");
-        schoolTypeMap.put("secundario", "Secondary");
-        schoolTypeMap.put("basico/secundario", "BasicSecondary");
+        schoolTypeMap.put("basico", POLIS_MPACT_NS + "Basic");
+        schoolTypeMap.put("secundario", POLIS_MPACT_NS + "Secondary");
+        schoolTypeMap.put("basico/secundario", POLIS_MPACT_NS + "BasicSecondary");
+        schoolTypeMap.put("secundario/basico", POLIS_MPACT_NS + "BasicSecondary");
     }
 
     public static String toEnglish(String entityName, String className) {
@@ -68,7 +70,7 @@ public class ToEnglish {
             return null;
         }
 
-        return POLIS_CORE_NS + englishName;
+        return englishName;
     }
 
     private static String normalize(String input) {
