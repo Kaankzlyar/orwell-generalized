22ac618443253dcefb1691f2d0102628ad4012f2
João Lourenço <jmguellourenco@gmail.com>
2026-05-04 20:40:19 +0100

    function: created strip party

---
+++ src/main/java/rdf/mapping/functions/StripParty.java
@@ -0,0 +1,14 @@
+package rdf.mapping.functions;
+
+public class StripParty {
+
+    public static String stripParty(String value) {
+        if (value == null) {
+            return null;
+        }
+        int idx = value.indexOf(" (");
+        String name = idx > 0 ? value.substring(0, idx) : null;
+        System.out.println("Stripped party: " + name);
+        return name;
+    }
+}
