4e266e468297be259ac2edfe2c850280bf09f252
João Lourenço <jmguellourenco@gmail.com>
2026-05-04 11:36:22 +0100

    mapping:use stringConcat isntead of template in receivedAppeal

--- src/main/java/rdf/mapping/functions/StringConcat.java
+++ src/main/java/rdf/mapping/functions/StringConcat.java
@@ -3,10 +3,9 @@ package rdf.mapping.functions;
 public class StringConcat {
 
     public static String stringConcat(String leftPart, String rightPart) {
-        
-        if(leftPart == null){
-            System.out.println("Right part: " + rightPart);
-        }
+        //if(leftPart == null){
+        //    System.out.println("Right part: " + rightPart);
+        //}
         // if(!rightPart.isEmpty() && rightPart.equals("8451")) {
         //     System.out.println("Left part: " + leftPart);
         //     System.out.println("Right part: " + rightPart);
