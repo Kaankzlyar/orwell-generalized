cd34bab38c68056f4c9949037f912ce168a40795
João Lourenço <jmguellourenco@gmail.com>
2026-05-05 01:35:34 +0100

    mapping: add unanimous

--- src/main/java/rdf/mapping/functions/StringEquals.java
+++ src/main/java/rdf/mapping/functions/StringEquals.java
@@ -4,7 +4,7 @@ public class StringEquals {
 
     public static Boolean stringEquals(String string1, String string2) {
         if (string1 == null || string2 == null) {
-            return null;
+            return false;
         }
 
         return string1.equals(string2);
